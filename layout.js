dd0e2bf471eb21de159b77e0919c7c91889c3ec2
biklav27 <tbiklav@gmail.com>
2026-07-18 17:09:47 +0545

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,15 @@
+import "./globals.css";
+import Header from "./common/Header";
+import Footer from "./common/Footer";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
