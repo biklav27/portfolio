3a91fbb9ddf700bada15c6cbd42e86a1123d40c3
biklav27 <tbiklav@gmail.com>
2026-07-18 17:11:34 +0545

    Add files via upload

--- page.js
+++ page.js
@@ -1,39 +1,52 @@
-import Image from "next/image";
-import { assets } from "./assets/image/assets";
-import Skills from "./skills/page";
-import Projects from "./projects/page";
+import React from "react";
 
-export default function Home() {
+function skills() {
   return (
-    <section>
-      {/* HERO SECTION */}
-      <section className="px-30 py-20 md:px-20 lg:px-40 md:py-20 lg:py-40 bg-[#011]">
-        <div className="grid gap-20 items-center justify-between md:flex ">
-          {/* text btn */}
-          <div className="flex flex-col gap-4 md:gap-6 text-white">
-            <h1 className="font-bold text-2xl md:text-4xl lg:text-6xl">Hello I'm Biklav</h1>
-            <p className="text-md md:text-xl lg:text-3xl">
-              A professional Front-End Developer creating modern UI <br /> and
-              smooth UX experiences.
+    // skills section
+    <section className="px-40 py-10 bg-[#099]">
+      <div className="">
+        <h1 className="text-white text-center text-4xl font-bold">My Skills</h1>
+        <div className="pt-5 grid grid-cols-3 items-center gap-[60px] ">
+          <div className="px-8 py-6 rounded-2xl border text-center shadow-lg bg-yellow-700 font-bold cursor-pointer">
+            <h2>HTML</h2>
+            <p>
+              Clean and semantic <br /> structure.
             </p>
-            <button className="px-4 py-2 w-40 bg-[#060] rounded-xl text-[#011] text-lg font-bold hover:bg-[#011] hover:text-[#7cf03d] hover:border hover:border-[#7cf03d]">
-              View Portfolio
-            </button>
           </div>
-          {/* profile */}
-          <div>
-            <Image
-              height={100}
-              width={150}
-              src={assets.profile2}
-              alt="profile"
-              className="rounded-full w-auto border-4 border-[#7cf03d]"
-            />
+          <div className="px-8 py-6 rounded-2xl border text-center shadow-lg bg-[#e6c681] font-bold cursor-pointer">
+            <h2>CSS</h2>
+            <p>
+              Morder UI with <br /> responsive design.
+            </p>
+          </div>
+          <div className="px-8 py-6 rounded-2xl border text-center shadow-lg bg-yellow-700 font-bold cursor-pointer">
+            <h2>JavaScript</h2>
+            <p>
+              Interactive and dynamic <br /> features.
+            </p>
+          </div>
+          <div className="px-8 py-6 rounded-2xl border text-center shadow-lg bg-yellow-700 font-bold cursor-pointer">
+            <h2>MYsql</h2>
+            <p>
+              Creating Database at <br /> high level in Mysql.
+            </p>
+          </div>
+          <div className="px-8 py-6 rounded-2xl border text-center shadow-lg bg-yellow-700 font-bold cursor-pointer">
+            <h2>React Js</h2>
+            <p>
+              Clean and semantic <br /> structure.
+            </p>
+          </div>
+          <div className="px-8 py-6 rounded-2xl border text-center shadow-lg bg-yellow-700 font-bold cursor-pointer">
+            <h2>GIT & GITHUB</h2>
+            <p>
+              GIT & GITHUB one years <br /> experiences.
+            </p>
           </div>
         </div>
-      </section>
-      {/* <Skills />
-      <Projects /> */}
+      </div>
     </section>
   );
 }
+
+export default skills;
